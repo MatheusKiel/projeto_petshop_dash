9461db98963d7887a91d5dfd53baabed3c2c8750
Matheus Kiel <matheuskiel@outlook.com>
2024-08-30 00:13:10 -0300

    .

--- code/testes_db.py
+++ code/testes_db.py
@@ -2,10 +2,10 @@ import pandas as pd
 import conexaodb as cdb
 
 # Inserts no banco de dados
-# cdb.inserir("INSERT INTO fornecedores (nome, contato) VALUES ('MedVet Mogi', 'Um endereço ai, 123')")
+# cdb.inserir("INSERT INTO fornecedores (nome, contato) VALUES ('MedVet', 'Um endereço ai, 123')")
 
 # Apagar dados no banco de dados
-# cdb.deletar("DELETE FROM fornecedores WHERE nome = 'MedVet Mogi'")
+# cdb.deletar("DELETE FROM fornecedores WHERE nome = 'MedVet'")
 
 # Alterar dados no banco de dados
 cdb.alterar("UPDATE fornecedores SET nome = 'Mudei de nome' WHERE id = 1")
